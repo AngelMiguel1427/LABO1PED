552ebdff71d9f51eb625d1c0402e8f3e544eb6f5
AngelMiguel1427 <00184026@uca.edu.sv>
2026-09-09 14:11:37 -0600

    menu para trabajar

---
+++ menu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "Bienvenido al menu de opciones" << endl;
+    cout << "seleccione la opcion deseada:" << endl;
+    cout << "Ver saldo" << endl;
+    cout << "2. Opcion 2" << endl;
+    cout << "1. Opcion 1" << endl;
+    cout << "2. Opcion 2" << endl;
+
+
+
+
+    return 0;
+}
